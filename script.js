113c0fdfd8840591daf76799a2a8686a5574be7f
Emerson Downing <57017788+EDowning2000@users.noreply.github.com>
2020-01-18 12:16:27 -0500

    Add files via upload
    Working password generator without copy and paste button

--- script.js
+++ script.js
@@ -4,18 +4,19 @@ var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P'
 var numbers = ['0','1','2','3','4','5','6','7','8','9'];
 var symbols = ['!','@','#','$','%','&','-','?'];
 var generateBtn = document.querySelector('#generate');
-var results = []
+var newPasswordFinal = []
 var userConditions = []
+var newPasswordRandom = []
+// var newPasswordFinal =[]
+
+function generatePassword(){
 
 //Math functions to find random characters
-var randomLowercase = lowercase[Math.floor(Math.random() * lowercase.length)]
-var randomUppercase = uppercase[Math.floor(Math.random() * uppercase.length)]
-var randomNumbers = numbers[Math.floor(Math.random() * numbers.length)]
-var randomSymbols = symbols[Math.floor(Math.random()*symbols.length)]
+
 
 // Assignment Code with prompts and confirms 
 var generateBtn = document.querySelector("#generate");
-var lengthPrompt= prompt('How many Characters long do you want the password to be?');
+var lengthPrompt= parseInt ( prompt ('How many Characters long do you want the password to be?'));
 var symbolsConfirm = confirm('Add symbols in your password?');
 var numbersConfirm = confirm('Add numbers in your password?');
 var uppercaseConfirm = confirm('Add uppercase letters in your password?');
@@ -24,22 +25,41 @@ var lowercaseConfirm = confirm('Add lowercase letters in your password?')
 
 
 //console logs testing out variables and prompts
-console.log(randomLowercase)
-console.log(randomSymbols)
-console.log(lengthPrompt)
-console.log(randomUppercase)
-console.log(randomNumbers)
-console.log(symbolsConfirm)
-console.log(numbersConfirm)
-console.log(uppercaseConfirm)
-console.log(lowercaseConfirm)
-console.log(userConditions)
+// console.log(randomLowercase)
+// console.log(randomSymbols)
+// console.log(lengthPrompt)
+// console.log(randomUppercase)
+// console.log(randomNumbers)
+// console.log(symbolsConfirm)
+// console.log(numbersConfirm)
+// console.log(uppercaseConfirm)
+// console.log(lowercaseConfirm)
+// console.log(userConditions)
 
 if(numbersConfirm){
-  
+  userConditions=userConditions.concat(numbers)
+}
+if(uppercaseConfirm){
+  userConditions=userConditions.concat(uppercase)
+}
+if(lowercaseConfirm){
+  userConditions=userConditions.concat(lowercase)
+}
+if(symbolsConfirm){
+  userConditions=userConditions.concat(symbols)
+}
+console.log(userConditions)
 
+for (var i=0; i < lengthPrompt; i++){
+  newPasswordRandom = userConditions[Math.floor(Math.random() * userConditions.length)]
+  console.log(newPasswordRandom);
+  
+  newPasswordFinal.push(newPasswordRandom)
 }
 
+return newPasswordFinal.join('')
+
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -48,8 +68,8 @@ function writePassword() {
 
   passwordText.value = password;
 
-  copyBtn.removeAttribute("disabled");
-  copyBtn.focus();
+  // copyBtn.removeAttribute("disabled");
+  // copyBtn.focus();
 }
 
 function copyToClipboard() {
